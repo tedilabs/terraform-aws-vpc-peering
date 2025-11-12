cc755c5d7d4ed8f6ed7978bf047d04a44c072b25
Byungjin Park <posquit0.bj@gmail.com>
2025-11-12 23:36:04 +0900

    chore: migrated examples

---
+++ examples/peering-cross-region/main.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc_one" {
+  source = "tedilabs/network/aws//modules/vpc"
+
+  name = "one"
+  ipv4_cidrs = [
+    {
+      type = "MANUAL"
+      cidr = "10.1.0.0/16"
+    }
+  ]
+
+  dns_hostnames_enabled = true
+  route53_resolver = {
+    enabled = true
+  }
+}
+
+module "vpc_two" {
+  source = "tedilabs/network/aws//modules/vpc"
+
+  region = "ap-northeast-2"
+  name   = "two"
+  ipv4_cidrs = [
+    {
+      type = "MANUAL"
+      cidr = "10.2.0.0/16"
+    }
+  ]
+
+  dns_hostnames_enabled = true
+  route53_resolver = {
+    enabled = true
+  }
+}
+
+
+###################################################
+# VPC Peering
+###################################################
+
+module "peering" {
+  source = "../../modules/peering"
+  # source  = "tedilabs/vpc-peering/aws//modules/peering"
+  # version = "~> 0.2.0"
+
+  providers = {
+    aws.requester = aws
+    aws.accepter  = aws
+  }
+
+  name = "use1/apne2"
+
+
+  ## Requester
+  requester = {
+    vpc = module.vpc_one.id
+  }
+  requester_options = {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+
+  ## Acccepter
+  accepter = {
+    vpc    = module.vpc_two.id
+    region = "ap-northeast-2"
+  }
+  accepter_options = {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+
+  tags = {
+    "project" = "terraform-aws-vpc-peering-examples"
+  }
+}
---
+++ examples/peering-simple/main.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc_one" {
+  source = "tedilabs/network/aws//modules/vpc"
+
+  name = "one"
+  ipv4_cidrs = [
+    {
+      type = "MANUAL"
+      cidr = "10.1.0.0/16"
+    }
+  ]
+
+  dns_hostnames_enabled = true
+  route53_resolver = {
+    enabled = true
+  }
+}
+
+module "vpc_two" {
+  source = "tedilabs/network/aws//modules/vpc"
+
+  name = "two"
+  ipv4_cidrs = [
+    {
+      type = "MANUAL"
+      cidr = "10.2.0.0/16"
+    }
+  ]
+
+  dns_hostnames_enabled = true
+  route53_resolver = {
+    enabled = true
+  }
+}
+
+
+###################################################
+# VPC Peering
+###################################################
+
+module "peering" {
+  source = "../../modules/peering"
+  # source  = "tedilabs/vpc-peering/aws//modules/peering"
+  # version = "~> 0.2.0"
+
+  providers = {
+    aws.requester = aws
+    aws.accepter  = aws
+  }
+
+  name = "one/two"
+
+
+  ## Requester
+  requester = {
+    vpc = module.vpc_one.id
+  }
+  requester_options = {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+
+  ## Acccepter
+  accepter = {
+    vpc = module.vpc_two.id
+  }
+  accepter_options = {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+
+  tags = {
+    "project" = "terraform-aws-vpc-peering-examples"
+  }
+}
